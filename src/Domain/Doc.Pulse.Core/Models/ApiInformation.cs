249e6e292c41c21a5feadc29e90928d355c75ba8
Troy Kammerdiener <github@kammerdiener.name>
2024-09-26 11:57:46 -0500

    feat(Core Entities): add ApiInformation Aggregate

---
+++ src/Domain/Doc.Pulse.Core/Models/ApiInformation.cs
@@ -0,0 +1,28 @@
+﻿using Doc.Pulse.Core.Abstractions;
+using Doc.Pulse.Core.Entities.ValueObjects;
+
+namespace Doc.Pulse.Core.Entities.ApiInformationAggregate;
+
+public partial class ApiInformation
+{
+    public ApiInformation() { }
+    public ApiInformation(string name, string description, string maintainer, string owner, EffectiveDate effective)
+    {
+        Name = name;
+        Description = description;
+        Maintainer = maintainer; 
+        Owner = owner;
+        Effective = effective;
+    }
+
+    public string? Name { get; private set; } = default;
+    public string? Description { get; private set; } = default;
+
+    public string? Maintainer { get; private set; }
+    public string? Owner { get; private set; }
+
+    public EffectiveDate? Effective { get; set; }
+
+
+    public static ApiInformation New(string name, string description, string maintainer, string owner, EffectiveDate effective) => new(name, description, maintainer, owner, effective);
+}
